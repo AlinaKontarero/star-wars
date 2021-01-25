8c9e8e32b8e234158b322665d4af5c3bc59b06b9
alina-kontarero <alina.kontarero@gmail.com>
2021-01-25 18:11:17 +1100

    added table draft

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,13 @@
 import * as React from 'react'
+import {
+  Column,
+  ColumnProps,
+  Table as VirtualizedTable,
+  TableCellProps,
+  SortDirectionType,
+  SortIndicator
+} from 'react-virtualized'
+import 'react-virtualized/styles.css'
 
 interface Props {
 
@@ -7,7 +16,25 @@ interface Props {
 class Table extends React.Component<Props, never> {
   render() {
     return (
-      <p>Hello! It would be a list here</p>
+      <VirtualizedTable
+        sort={this.props.onSort}
+        width={width}
+        height={this.getHeight()}
+        headerHeight={this.props.headerHeight}
+        rowHeight={this.props.rowHeight}
+        rowCount={this.getRowCount()}
+        rowGetter={({ index }) => this.props.data[index]}
+        className={`pendula-table ${this.props.className || ''}`}
+      >
+        {this.props.columns.map(columnProps => (
+          <Column
+            key={`tablecolumn-${columnProps.dataKey}`}
+            {...columnProps}
+            cellRenderer={this.getCellRenderer(columnProps)}
+            headerRenderer={() => this.headerRenderer(columnProps)}
+          />
+        ))}
+      </VirtualizedTable>
     )
   }
 }
