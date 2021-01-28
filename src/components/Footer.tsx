0596c56585d0bc4b505be5e144ba2cde9359024b
alina-kontarero <alina.kontarero@gmail.com>
2021-01-29 10:09:51 +1100

    Updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
 
-const Footer = () => {return (
-    <div className='column is-footer'>
-      <p className="center">&copy; Test challenge completed by <a href='https://www.linkedin.com/in/alina-kontarero/'>Aline Kontarero</a> for the Sympli team <br /> {new Date().getFullYear()}</p>
-    </div>
-  )}
+const Footer = () =>  (
+    <footer className='column is-full is-footer footer'>
+      <div className="content has-text-centered">
+      <p className="">&copy; Test challenge completed by <a href='https://www.linkedin.com/in/alina-kontarero/'>Aline Kontarero</a> for the Sympli team <br /> {new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  )
 
 export default Footer;
